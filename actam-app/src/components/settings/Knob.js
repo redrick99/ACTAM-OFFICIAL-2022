87c89d93889b483b6895c83074bff5ff95082f74
francescopiferi99 <francesco.piferi@mail.polimi.it>
2022-08-06 12:12:14 +0200

    Knob modifications

--- actam-app/src/components/settings/Knob.js
+++ actam-app/src/components/settings/Knob.js
@@ -8,22 +8,25 @@ class Knob extends Component {
   constructor(props) {
     super(props);
 
-    this.change = this.change.bind(this);
+    // Binding all functions to "this"
     this.mouseDown = this.mouseDown.bind(this);
     this.mouseUp = this.mouseUp.bind(this);
     this.mouseLeave = this.mouseLeave.bind(this);
-
+    this.changeValue = this.changeValue.bind(this);
     this.rotate = this.rotate.bind(this);
   }
-
+  /**
+   * Every knob has a private state to monitor if the knob is or not active and the value of itself
+   */
   state = {
     isClicked: false,
+    value: 0,
   };
 
-  change(event) {
-    this.props.change(event.target.value, this.props.idNumber);
-  }
-
+  /**
+   * If the knob is clicked, state.isClicked turns to true
+   * @param {click} event
+   */
   mouseDown(event) {
     if (!this.state.isClicked) {
       this.setState(() => ({
@@ -32,22 +35,32 @@ class Knob extends Component {
     }
   }
 
+  /**
+   * When the mouse is released state.isClicked turns to false
+   * @param {mouse release} event
+   */
   mouseUp(event) {
     if (this.state.isClicked) {
       this.setState(() => ({
         isClicked: false,
       }));
-
-      console.log("ESCOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
     }
   }
 
+  /**
+   * When the mouse leaves the component state.isClicked turns to false
+   * @param {mouse leave} event
+   */
   mouseLeave(event) {
     if (this.state.isClicked) {
       this.mouseUp();
     }
   }
 
+  /**
+   * When the mouse is moved this function handle the rotation of the knob image basing it on the position of the mouse
+   * @param {mouse move} event
+   */
   rotate(event) {
     if (this.state.isClicked) {
       const knobPos = event.target.getBoundingClientRect();
@@ -67,40 +80,87 @@ class Knob extends Component {
       } else {
         deg += 90;
       }
-
       if (deg < 135 || deg > 225) {
-        event.target.style.transform = "translate(0%, -100%) rotate(" + deg + "deg)";
+        event.target.style.transform = "translate(35%, -160%) rotate(" + deg + "deg)";
+        if (deg > 225 && deg <= 360) {
+          deg = deg / 360 - 225 / 360;
+          if (deg >= 1) deg = 0;
+        } else {
+          deg = deg / 360 + 1 - 225 / 360;
+        }
+        deg = ((deg * 4) / 3) * (this.props.maxRange - this.props.minRange) + this.props.minRange;
+        this.props.change(deg);
+        this.setState(() => ({
+          value: deg,
+        }));
       } else this.mouseUp();
-
-      //this.props.rotate(deg);
     }
   }
 
+  /**
+   * After the render the position of the knob is set to the one specified on the props
+   */
   componentDidMount() {
-    document.getElementById(this.props.id).style.transform = "translate(0%, -100%) rotate(225deg)";
+    var unity;
+    var initialDeg = this.props.initial;
+    unity = 135 / ((this.props.maxRange - this.props.minRange) / 2);
+    initialDeg = unity * initialDeg - 135 - this.props.minRange * unity;
+    document.getElementById(this.props.id).style.transform = "translate(35%, -160%) rotate(" + initialDeg + "deg)";
+    this.setState(() => ({
+      value: this.props.initial,
+    }));
+  }
+
+  /**
+   *
+   * @param {input change} event
+   */
+  changeValue(event) {
+    if (
+      (event.target.value >= this.props.minRange && event.target.value <= this.props.maxRange) ||
+      event.target.value == 0
+    ) {
+      var initialDeg = event.target.value;
+      var unity;
+      unity = 135 / ((this.props.maxRange - this.props.minRange) / 2);
+      initialDeg = unity * initialDeg - 135 - this.props.minRange * unity;
+      document.getElementById(this.props.id).style.transform = "translate(35%, -160%) rotate(" + initialDeg + "deg)";
+      this.setState(() => ({
+        value: event.target.value,
+      }));
+    }
   }
 
   render() {
     return (
-      <div className={this.props.className}>
+      <React.Fragment>
+        <label htmlFor={this.props.id}>{this.props.name}</label>
         <div>
-          <label htmlFor={this.props.id}>{this.props.name}</label>
-          <div className="imgKnobIndicator"></div>
-          <div
-            className="imgKnob"
-            onMouseDown={this.mouseDown}
-            onMouseUp={this.mouseUp}
-            onMouseMove={this.rotate}
-            onDrag={(e) => {
-              e.preventDefault();
-            }}
-            onDragStart={(e) => e.preventDefault()}
-            onDragEnd={(e) => e.preventDefault()}
-            onMouseLeave={this.mouseLeave}
-            id={this.props.id}
+          <input
+            onInputCapture={this.changeValue}
+            id={"input" + this.props.id}
+            value={this.state.value.toString().slice(0, 4)}
+            onChange={this.changeValue}
+            className="inputField"
           />
         </div>
-      </div>
+
+        <div className={"imgKnobIndicator " + this.props.img}></div>
+
+        <div
+          className="imgKnob"
+          onMouseDown={this.mouseDown}
+          onMouseUp={this.mouseUp}
+          onMouseMove={this.rotate}
+          onDrag={(e) => {
+            e.preventDefault();
+          }}
+          onDragStart={(e) => e.preventDefault()}
+          onDragEnd={(e) => e.preventDefault()}
+          onMouseLeave={this.mouseLeave}
+          id={this.props.id}
+        />
+      </React.Fragment>
     );
   }
 }
--- actam-app/src/components/settings/KnobHandler.js
+++ actam-app/src/components/settings/KnobHandler.js
@@ -1,44 +1,90 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import Knob from "./Knob";
 
 class KnobHandler extends Component {
   constructor(props) {
     super(props);
-    this.change = this.change.bind(this);
-  }
-
-  state = {
-    knobs: [
-      { id: 0, value: 50, name: "volume" },
-      { id: 1, value: 50, name: "wet" },
-      { id: 2, value: 50, name: "harmonic" },
-      { id: 3, value: 50, name: "voiceTension" },
-    ],
-  };
-
-  change(value, id) {
-    const newKnobs = [...this.state.knobs];
-    newKnobs[id].value = value;
-
-    this.setState(() => ({
-      knobs: newKnobs,
-    }));
   }
 
   render() {
     return (
-      <div>
-        {this.state.knobs.map((knob, index) => (
-          <Knob
-            key={index}
-            idNumber={knob.id}
-            dispText={knob.value}
-            change={this.change}
-            name={knob.name}
-            id={"knob-" + index}
-          ></Knob>
-        ))}
+      <div className="divCont">
+        <table className="controller" height="190px">
+          <tbody height="190px">
+            <tr height="190px">
+              <td height="190px">
+                <Knob
+                  key="0"
+                  idNumber={0}
+                  dispText={this.props.value}
+                  change={this.props.change[0]}
+                  name="volume"
+                  id={"knob-0"}
+                  minRange={0}
+                  maxRange={10}
+                  initial={3}
+                  img="k0-10"
+                ></Knob>
+              </td>
+              <td height="190px">
+                <Knob
+                  key="1"
+                  idNumber={1}
+                  dispText={this.props.value}
+                  change={this.props.change[1]}
+                  name="chorous"
+                  id={"knob-1"}
+                  minRange={0}
+                  maxRange={1}
+                  initial={0.1}
+                  img="k0-1-2"
+                ></Knob>
+              </td>
+              <td height="190px">
+                <Knob
+                  key="2"
+                  idNumber={2}
+                  dispText={this.props.value}
+                  change={this.props.change[2]}
+                  name="reverb"
+                  id={"knob-2"}
+                  minRange={0}
+                  maxRange={1}
+                  initial={0.7}
+                  img="k0-10"
+                ></Knob>
+              </td>
+              <td height="190px">
+                <Knob
+                  key="3"
+                  idNumber={3}
+                  dispText={this.props.value}
+                  change={this.props.change[3]}
+                  name="attack"
+                  id={"knob-3"}
+                  minRange={0}
+                  maxRange={1}
+                  initial={0.7}
+                  img="k0-10"
+                ></Knob>
+              </td>
+              <td height="190px">
+                <Knob
+                  key="4"
+                  idNumber={4}
+                  dispText={this.props.value}
+                  change={this.props.change[4]}
+                  name="release"
+                  id={"knob-4"}
+                  minRange={0}
+                  maxRange={100}
+                  initial={90}
+                  img="k0-10"
+                ></Knob>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
