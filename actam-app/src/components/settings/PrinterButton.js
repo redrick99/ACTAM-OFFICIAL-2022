a650e716fede579cf409111d45b04f822503c4be
francescopiferi99 <francesco.piferi@mail.polimi.it>
2022-09-03 15:43:41 +0200

    Print complete

--- actam-app/src/components/settings/PrinterButton.js
+++ actam-app/src/components/settings/PrinterButton.js
@@ -1,11 +1,152 @@
 import React, { Component } from "react";
+
+import { assignables, modalScalesTextUpper, rootKeys, voicingNames, voicingType } from "../../scripts/GlobalVariables";
+import chordsFactory from "../../scripts/Chords/ChordsFactory";
+
 class PrinterButton extends Component {
   constructor(props) {
     super(props);
+    this.print = this.print.bind(this);
+    this.writePage = this.writePage.bind(this);
+    this.scoreContainer = undefined;
+  }
+
+  print() {
+    /*
+    const printingChord = chordProgressionHandler.getChordsForPrint(assignables.chords, false);
+    const effettiveChords = voicingsHandler.getVoicings(printingChord[0].chord, printingChord[0].duration);
+    */
+
+    const effettiveChords = chordsFactory.getChordsToPrint(
+      assignables.chords,
+      1,
+      assignables.selectedName,
+      assignables.selectedType
+    );
+
+    let scope = document.createElement("div");
+    let tableHtmlContent = document.createElement("h1");
+    tableHtmlContent.textContent = "Voicing Generator";
+    let keySignature = document.createElement("h3");
+    keySignature.textContent = "Key Signature: " + rootKeys[assignables.currentKey];
+    let modalContent = document.createElement("h3");
+    modalContent.textContent = "Modal Scale: " + modalScalesTextUpper[assignables.currentMode];
+    let voiceNameContent = document.createElement("h3");
+    voiceNameContent.textContent = "Voicing Name: " + voicingNames[assignables.selectedName];
+    let voiceTypeContent = document.createElement("h3");
+    voiceTypeContent.textContent = "Voicing Type: " + voicingType[assignables.selectedType];
+    let chordScope = document.createElement("h3");
+    let i = 0;
+    let chordsList = "";
+    let [treble, bass] = [true, false];
+
+    while (effettiveChords[i] != null) {
+      if (i === 0) {
+        chordsList += effettiveChords[i].name;
+      } else {
+        chordsList += ", " + effettiveChords[i].name;
+      }
+      i++;
+    }
+    chordScope.textContent = "Chords List: " + chordsList;
+
+    i = 0;
+    let scoreCont = document.createElement("table");
+    let tbody = scoreCont.appendChild(document.createElement("tbody"));
+    let trow;
+    let tcell;
+    let innerCellTreble;
+    let innerRowTreble;
+    let innerCellBass;
+    let innerRowBass;
+    let label;
+    let labelRow;
+    let innerTable;
+    let innerTbody;
+    while (effettiveChords[i] != null) {
+      if (i % 4 === 0) {
+        trow = tbody.appendChild(document.createElement("tr"));
+      }
+      tcell = trow.appendChild(document.createElement("td"));
+      tcell.style.border = "3px solid black";
+      tcell.style.borderRadius = "5px";
+      innerTable = tcell.appendChild(document.createElement("table"));
+      innerTbody = innerTable.appendChild(document.createElement("tbody"));
+      labelRow = innerTbody.appendChild(document.createElement("tr"));
+      innerRowTreble = innerTbody.appendChild(document.createElement("tr"));
+      innerCellTreble = innerRowTreble.appendChild(document.createElement("td"));
+      innerRowBass = innerTbody.appendChild(document.createElement("tr"));
+      innerCellBass = innerRowBass.appendChild(document.createElement("td"));
+      label = labelRow.appendChild(document.createElement("td"));
+
+      [treble, bass] = effettiveChords[i].drawScore({ treble: innerCellTreble, bass: innerCellBass });
+
+      if (!treble) {
+        innerCellTreble.classList.add("hidden");
+      }
+      if (!bass) {
+        innerCellBass.classList.add("hidden");
+      }
+
+      //effettiveChords[i].renderScore(innerCell, scoreOptions);
+      label.textContent = effettiveChords[i].name;
+      i++;
+    }
+
+    scope.appendChild(tableHtmlContent);
+    scope.appendChild(keySignature);
+    scope.appendChild(modalContent);
+    scope.appendChild(voiceNameContent);
+    scope.appendChild(voiceTypeContent);
+    scope.appendChild(chordScope);
+    scope.appendChild(scoreCont);
+
+    /*
+    let tableHtmlContent =
+      "<h1 className='title'>Voicing Generator</h1><h3>Key Signature: " + rootKeys[assignables.currentKey] + "</h3>";
+    tableHtmlContent += "<h3>Modal Scale: " + modalScalesText[assignables.currentMode] + "</h3>";
+    tableHtmlContent += "<h3>Voicing Name: " + voicingNames[assignables.currentVoicingName] + "</h3>";
+    tableHtmlContent += "<h3>Voicing Type: " + voicingType[assignables.currentVoicingType] + "</h3>";
+    let i = 0;
+    tableHtmlContent += "<h3>Chords List: ";
+    while (effettiveChords[i] != null) {
+      tableHtmlContent += effettiveChords[i].name + ", ";
+      i++;
+    }
+    tableHtmlContent += "</h3>";
+
+    // tableHtmlContent += '<div id="scoreCont></div>';
+    // this.scoreContainer = document.getElementById("scoreCont");
+    let score = document.createElement("div");
+    score.id = "scoreCont";
+    */
+
+    //effettiveChords[0].renderScore(this.scoreContainer, scoreOptions);
+
+    let oHideFrame = document.createElement("iframe");
+    oHideFrame.style.position = "fixed";
+    oHideFrame.style.right = "0";
+    oHideFrame.style.bottom = "0";
+    oHideFrame.style.width = "0";
+    oHideFrame.style.height = "0";
+    oHideFrame.style.border = "0";
+    document.body.appendChild(oHideFrame);
+    oHideFrame.appendChild(scope);
+
+    let oDoc = oHideFrame.contentWindow || oHideFrame.contentDocument;
+    if (oDoc.document) oDoc = oDoc.document;
+    this.writePage(oDoc, oHideFrame.innerHTML);
+    oDoc.close();
+  }
+
+  writePage(document, innerHTML) {
+    document.write('<head><link type="text/css" rel="stylesheet" href="../../App.css"><title>Chords</title></head>');
+    document.write('<body onload="this.focus(); this.print();">');
+    document.write(innerHTML + "</body>");
   }
 
   render() {
-    return <div title="Print the chords" className={"printer-button "} onClick={this.props.print} />;
+    return <div title="Print the chords" className={"printer-button "} onClick={this.print} />;
   }
 }
 
--- actam-app/src/scripts/GlobalVariables.js
+++ actam-app/src/scripts/GlobalVariables.js
@@ -1,4 +1,4 @@
-import * as Tone from 'tone';
+import * as Tone from "tone";
 import { Tonalities as T } from "./Tonalities";
 
 export const rootKeys = "C C# D D# E F F# G G# A A# B".split(" ");
@@ -7,74 +7,83 @@ export const basicProgression = "I II III IV V VI VII".split(" ");
 export const majorScale = [T.MAJ, T.MIN, T.MIN, T.MAJ, T.DOM, T.MIN, T.HDIM];
 export const ionianTonalDistance = [2, 2, 1, 2, 2, 2, 1];
 export const modalScalesText = "ionian dorian phrygian lydian mixolydian aeolian locrian".split(" ");
-export const voicingsTypes = "Rootless A, Rootless B, Monk, Powell 1, Powell 2, Powell 3, Powell 4, Three Notes, Four Notes, Open Chord 1, Open Chord 2".split(", ");
+export const modalScalesTextUpper = "Ionian Dorian Phrygian Lydian Mixolydian Aeolian Locrian".split(" ");
+export const voicingsTypes =
+  "Rootless A, Rootless B, Monk, Powell 1, Powell 2, Powell 3, Powell 4, Three Notes, Four Notes, Open Chord 1, Open Chord 2".split(
+    ", "
+  );
 
 export const modalScales = [];
 export const scalesDistances = [];
-for(let i = 0; i < 7; i++) {
-    modalScales[i] = majorScale.slice(i, majorScale.length).concat(majorScale.slice(0, i));
-    scalesDistances[i] = ionianTonalDistance.slice(i, ionianTonalDistance.length).concat(ionianTonalDistance.slice(0, i));
+for (let i = 0; i < 7; i++) {
+  modalScales[i] = majorScale.slice(i, majorScale.length).concat(majorScale.slice(0, i));
+  scalesDistances[i] = ionianTonalDistance.slice(i, ionianTonalDistance.length).concat(ionianTonalDistance.slice(0, i));
 }
+
+export const voicingNames = "Rootless, Monk, Powell, Three Notes, Four Notes, Open Chord".split(", ");
+export const voicingType = "Type 1, Type 2, Type 3, Type 4".split(", ");
 
 export const chordTonalityMap = new Map();
 chordTonalityMap.set("△", [0, 4, 7, 11]);
 chordTonalityMap.set("-7", [0, 3, 7, 10]);
 chordTonalityMap.set("7", [0, 4, 7, 10]);
-chordTonalityMap.set("-7♭5", [0, 3, 6, 10])
+chordTonalityMap.set("-7♭5", [0, 3, 6, 10]);
 
-export const master = new Tone.Gain()
-master.gain.value=0.5;
+export const master = new Tone.Gain();
+master.gain.value = 0.5;
 
 export const assignables = {
-    currentKey: 0,
-    currentMode: 0,
-    currentProgression: Array.from(basicProgression, (element, index) => element + majorScale[index]),
-    currentModalIntervals: [0, 0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23],
-    chords: Array(16).join(".").split("."),
-    bpm: 60,
-    loop: false,
-    legato: false,
-    octaveShift: 0,
-    selectedName: 0,
-    selectedType: 0,
-    waitingFunction: undefined,
-    loadingBarFunction: undefined,
-}
+  currentKey: 0,
+  currentMode: 0,
+  currentVoicingName: 0,
+  currentVoicingType: 0,
+  currentProgression: Array.from(basicProgression, (element, index) => element + majorScale[index]),
+  currentModalIntervals: [0, 0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23],
+  chords: Array(16).join(".").split("."),
+  bpm: 60,
+  loop: false,
+  legato: false,
+  octaveShift: 0,
+  selectedName: 0,
+  selectedType: 0,
+  waitingFunction: undefined,
+  loadingBarFunction: undefined,
+};
 
 export const scoreOptions = {
-    treble: {
-        rWidth: 160,
-        rHeight: 110,
-        sPosX: 20,
-        sPosY: 0,
-        sWidth: 130,
-    },
-    bass: {
-        rWidth: 160,
-        rHeight: 110,
-        sPosX: 20,
-        sPosY: 20,
-        sWidth: 130,
-    },
-    onlyOne: {
-        rWidth: 160,
-        rHeight: 228,
-        sPosX: 20,
-        sPosY: 40,
-        sWidth: 130,
-    },
+  treble: {
     rWidth: 160,
-    rHeight: 208,
+    rHeight: 110,
+    sPosX: 20,
+    sPosY: 0,
+    sWidth: 130,
+  },
+  bass: {
+    rWidth: 160,
+    rHeight: 110,
+    sPosX: 20,
+    sPosY: 20,
+    sWidth: 130,
+  },
+  onlyOne: {
+    rWidth: 160,
+    rHeight: 228,
     sPosX: 20,
     sPosY: 40,
     sWidth: 130,
-}
+  },
+  rWidth: 160,
+  rHeight: 208,
+  sPosX: 20,
+  sPosY: 40,
+  sWidth: 130,
+};
 
 export const functions = {
-    getTonalityOfChord: function(grade) {
-        return (modalScales[assignables.currentMode])[grade];
-    },
-    getChordSymbol: function(fundamental, tonality) {
-        return rootKeys[fundamental % 12] + tonality;
-    },
-}
+  getTonalityOfChord: function (grade) {
+    return modalScales[assignables.currentMode][grade];
+  },
+  getChordSymbol: function (fundamental, tonality) {
+    return rootKeys[fundamental % 12] + tonality;
+  },
+};
